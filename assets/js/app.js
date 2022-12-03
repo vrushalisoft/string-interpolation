fc73e5595183d151457e379a77820c63e2c87d36
vrushalisoft <57787855+vrushalisoft@users.noreply.github.com>
2022-12-03 16:25:33 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,130 @@
+// alert("Hello");
+
+//1) Hello there, I'm Vrushali Thombare, 33 years old. I'm from Pune and MEAN Stack Developer by profession.
+var fname = "Vrushali";
+var lname = "Thombare";
+var age = 33;
+var nativePlace = "Pune";
+var profile = "MEAN Stack Developer";
+
+var result = "1) Hello there, I'm " + fname + " " + lname +", " + age +
+            " years old. I'm from " + nativePlace + " and " + profile +
+            " by profession.<br>";
+
+console.log(result);
+document.write(result);
+
+
+// 2)Software is a set of computer programs and associated documentation and data. 
+// This is in contrast to hardware, from which the system is built and which actually performs the work.
+var name = "Software";
+var info = "computer programs and associated documentation and data";
+var type  =  "contrast";
+var work = "Work";
+
+var result = "2) " + name + " is a set of " + info +". " +
+            "This is in " + type + " to hardware, from which the system is built and which actually performs the " 
+            + work + ".<br>";
+
+console.log(result);
+document.write(result);
+
+
+// 3)Angular is a typescript-based web application framework that supports full-stack development for building all types of web applications. 
+// It helps in creating reactive single page application (SPA) and is completely based on the concept of components. Google owns Angular,
+//  and its stable version was released on September 14, 2016.
+var techName = "Angular";
+var development = "full-stack development";
+var application  =  "single page application (SPA)";
+var year = 2016;
+
+var result = "3) " + techName + " is a typescript-based web application framework that supports " + development + " for building all types of web applications. It helps in creating reactive " + application +
+            " and is completely based on the concept of components. Google owns" + techName + ", and its stable version was released on September 14, " + year + ".<br>";
+
+console.log(result);
+document.write(result);
+
+
+// 4) CSS is the language we use to style an HTML document. CSS describes how HTML elements should be displayed.
+var langName1 = "CSS";
+var langName2 = "HTML";
+
+var result = "4) " + langName1 + " is the language we use to style an " + langName2 + " document. " + langName1 + " describes how " + langName2 + " elements should be displayed.<br>";
+
+console.log(result);
+document.write(result);
+
+//5)Avul Pakir Jainulabdeen Abdul Kalam was an Indian aerospace scientist and statesman who served as the 11th President of India from 2002 to 2007. 
+var fullName = "Avul Pakir Jainulabdeen Abdul Kalam";
+var profession1 = "aerospace scientist";
+var profession2 = "statesman";
+var startYear = 2002;
+var endYear = 2007;
+
+
+var result = "5) " + fullName + " was an Indian " + profession1 + " and " + profession2 +  " who served as the 11th President of India from " + startYear + " to " + endYear + ".<br>";
+
+console.log(result);
+document.write(result);
+
+// 6)Independence Day marks the end of British rule in 1947 and the establishment of a free and independent Indian nation.
+var day = "Independence Day";
+var year = 1947;
+var nation = "Indian";
+
+var result = "6) " + day + " marks the end of British rule in " + year + " and the establishment of a free and independent " + nation + " nation.<br>";
+
+console.log(result);
+document.write(result);
+
+// 7) On 14 November 1987, the 14-year-old Tendulkar was selected to represent Bombay in the Ranji Trophy, India's premier domestic First-class cricket tournament, for the 1987–88 season.
+var day = "14 November 1987";
+var age  =  14;
+var locate  =  "Bombay";
+var year =  1987-88;
+
+var result = "7) On " + day + " the " + age + "-year-old Tendulkar was selected to represent " + locate + " in the Ranji Trophy, India's premier domestic First-class cricket tournament, for the " + year + " season.<br>";
+
+console.log(result);
+document.write(result);
+
+//8) Lata Mangeshkar was an Indian playback singer and occasional music composer. 
+// She is widely considered to have been one of the greatest and most influential singers in India.
+// Her contribution to the Indian music industry in a career spanning eight decades gained her honorific titles such as the "Queen of Melody", "Nightingale of India", and "Voice of the Millennium".
+
+var fullName = "Lata Mangeshkar";
+var profession1  =  "singer";
+var profession2  =  "composer";
+var nationality  =  "Indian";
+var titles =  '"Queen of Melody", "Nightingale of India", and "Voice of the Millennium"';
+
+var result = "8) " + fullName + " was an " + nationality + " playback " + profession1 + "  and occasional music " + profession2 + ". She is widely considered to have been one of the greatest and most influential singers in India. Her contribution to the "
+            + nationality + " music industry in a career spanning eight decades gained her honorific titles such as the " + titles +".<br>"
+
+console.log(result);
+document.write(result);
+
+//9)Shivaji, born February 19, 1630, Shivneri, Pune, founder of the Maratha kingdom of India. 
+// The kingdom’s security was based on religious toleration and on the functional integration of the Brahmans, Marathas, and Prabhus.
+
+var fname = "Shivaji";
+var year  =  1630;
+var title  =  "the Maratha kingdom of India";
+var religion =  'Brahmans, Marathas, and Prabhus';
+
+var result = "9) " + fname + " , born February 19, " + year + ", Shivneri, Pune, founder of the " + title + ". The kingdom’s security was based on religious toleration and on the functional integration of the " + religion +".<br>"
+
+console.log(result);
+document.write(result);
+
+//10)India, officially the Republic of India is a country in South Asia.
+//  It is the seventh-largest country by area, the second-most populous country, and the most populous democracy in the world.
+
+var country = "India";
+var locate  =  "South Asia";
+var demo  =  "democracy";
+
+var result = "10) " + country + ", officially the Republic of India is a country in " + locate + ". It is the seventh-largest country by area, the second-most populous country, and the most " + demo + " in the world.<br>"
+
+console.log(result);
+document.write(result);
